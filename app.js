62bf9b2f021897eb7b55978b755cb13c574d53fc
magokoro2024 <magokoro2024.4@gmail.com>
2025-07-09 15:24:06 +0900

    Add debug webhook endpoint to troubleshoot 404 issues

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ルーティング設定
 app.use('/api/line', require('./routes/line'));
+app.use('/api/debug', require('./routes/webhook-debug'));
 app.use('/api/reservations', require('./routes/reservations'));
 
 // ビューエンジン設定
